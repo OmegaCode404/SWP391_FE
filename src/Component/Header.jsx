bbe583a12796a44f7d0c8ffb3bcd767d872fd2ba
Lam-Hoang-Gia-Huy <huylhgse173548fpt.edu.vn>
2024-05-15 22:43:51 +0700

    update mainpage

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,10 +1,25 @@
 import { Menu } from "antd";
 import { Header } from "antd/es/layout/layout";
+import { AudioOutlined } from "@ant-design/icons";
+import { Input, Space } from "antd";
+import { Col, Row } from "antd";
+import { Image } from "antd";
+
+const { Search } = Input;
 const labels = ["Home", "About us", "Login"];
 const items = labels.map((label, index) => ({
   key: String(index + 1),
   label,
 }));
+const suffix = (
+  <AudioOutlined
+    style={{
+      fontSize: 16,
+      color: "#1677ff",
+    }}
+  />
+);
+const onSearch = (value, _e, info) => console.log(info?.source, value);
 const HeaderBar = () => {
   return (
     <Header
@@ -17,17 +32,32 @@ const HeaderBar = () => {
         alignItems: "center",
       }}
     >
-      <div className="demo-logo" />
-      <Menu
-        theme="dark"
-        mode="horizontal"
-        defaultSelectedKeys={["2"]}
-        items={items}
-        style={{
-          flex: 1,
-          minWidth: 0,
-        }}
-      />
+      <Col span={3}>
+        <Image
+          src="https://placeholderlogo.com/img/placeholder-logo-1.png"
+          height={60}
+        />
+      </Col>
+      <Col span={5}>
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={["2"]}
+          items={items}
+          style={{
+            flex: 1,
+            minWidth: 0,
+          }}
+        />
+      </Col>
+      <Col span={16}>
+        <Search
+          placeholder="Input watch name here"
+          onSearch={onSearch}
+          enterButton
+          style={{ marginTop: "15px", width: "60%" }}
+        />
+      </Col>
     </Header>
   );
 };
--- src/Component/ItemList.jsx
+++ src/Component/ItemList.jsx
@@ -1,22 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Card, Col, Row } from "antd";
-const ItemList = () => (
-  <Row gutter={16}>
-    <Col span={8}>
-      <Card hoverable title="Card title" bordered={false}>
-        Card content
-      </Card>
-    </Col>
-    <Col span={8}>
-      <Card hoverable title="Card title" bordered={false}>
-        Card content
-      </Card>
-    </Col>
-    <Col span={8}>
-      <Card hoverable title="Card title" bordered={false}>
-        Card content
-      </Card>
-    </Col>
-  </Row>
-);
+import axios from "axios";
+
+const ItemList = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://6644a330b8925626f88f3fb9.mockapi.io/api/v1/watch"
+        );
+        setItems(response.data);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <Row gutter={16}>
+      {items.map((item) => (
+        <Col key={item.id} span={8}>
+          <Card
+            hoverable
+            title={item.name}
+            bordered={true}
+            cover={
+              <img
+                alt={item.name}
+                src={item.url}
+                className="item-cover-image"
+              />
+            }
+          >
+            <span className="item-price">
+              Price: {item.price.toLocaleString()} đ
+            </span>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  );
+};
+
 export default ItemList;
